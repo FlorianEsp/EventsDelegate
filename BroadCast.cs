66c835ed8c3cda74e37a1e295d20607992a4bc6d
Florian Espriet <Florian.Espriet@student.intecbrussel.be>
2022-03-15 14:05:06 +0100

    Add project files.

---
+++ BroadCast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegate
+{
+    public class BroadCast
+    {
+        public DateTime DatePublished { get; set; }
+        public string PublisherName { get; set; }
+    }
+}
---
+++ NewsLetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegate
+{
+    public class NewsLetter:EventArgs 
+    {
+        public delegate void EventHandler(NewsLetter publisher,BroadCast e);
+        public event EventHandler NewArticle;
+        
+        public void OnNewArticle()
+        {
+            if (true)
+            {
+
+            }
+        }
+    }
+}
